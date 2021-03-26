9af67f6744cef0aa9761d811d7317e7fbe15da7d
Nitish Devadiga <nitishdevadiga@hotmail.com>
2021-03-27 01:03:08 +0530

    + Added Mouse Control + Added Background Subtraction Function (Not accurate at the moment) + Moved some functions to utilities.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import numpy as np
 import cv2 as cv
+import pyautogui
 from matplotlib import pyplot as plt
+import utilities
 
-def nothing(x=None):
-    pass
+pyautogui.FAILSAFE = False
+screenWidth, screenHeight = pyautogui.size()
+
+enableMouseTracking = False
 
 cap = cv.VideoCapture(0)
 
@@ -16,40 +20,70 @@ cv.namedWindow('frame')
 cap.set(3, 1280)
 cap.set(4, 720)
 
+paddingPercent = 30
+trackingRangeX = [0 + (paddingPercent/100)*(cap.get(cv.CAP_PROP_FRAME_WIDTH)), (100-paddingPercent)/100*(cap.get(cv.CAP_PROP_FRAME_WIDTH))]
+trackingRangeY = [0 + (paddingPercent/100)*(cap.get(cv.CAP_PROP_FRAME_HEIGHT)), (100-paddingPercent)/100*(cap.get(cv.CAP_PROP_FRAME_HEIGHT))]
+
 resolution = 'Width: ' + str(cap.get(cv.CAP_PROP_FRAME_WIDTH)) + ' Height: ' + str(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
 contrast = 10
-def updateContrast(x):
+def update_contrast(x):
     global contrast
     contrast = x
 
 brightness = 128
-def updateBrightness(x):
+def update_brightness(x):
     global brightness
     brightness = x
 
-cv.createTrackbar('Contrast', 'frame', 10, 30, updateContrast)
-cv.createTrackbar('Brightness', 'frame', 128, 256, updateBrightness)
+sensitivity = 100
+def update_sensitivity(x):
+    global sensitivity
+    sensitivity = x
+
+cv.createTrackbar('Contrast', 'frame', 10, 30, update_contrast)
+cv.createTrackbar('Brightness', 'frame', 128, 256, update_brightness)
+cv.createTrackbar('Sensitivity', 'frame', 100, 1000, update_sensitivity)
 
 cv.namedWindow('Tracking')
 
-cv.createTrackbar('LH', 'Tracking', 12, 255, nothing)
-cv.createTrackbar('LS', 'Tracking', 100, 255, nothing)
-cv.createTrackbar('LV', 'Tracking', 100, 255, nothing)
+cv.createTrackbar('LH', 'Tracking', 12, 255, utilities.nothing)
+cv.createTrackbar('LS', 'Tracking', 100, 255, utilities.nothing)
+cv.createTrackbar('LV', 'Tracking', 100, 255, utilities.nothing)
 
-cv.createTrackbar('UH', 'Tracking', 35, 255, nothing)
-cv.createTrackbar('US', 'Tracking', 255, 255, nothing)
-cv.createTrackbar('UV', 'Tracking', 255, 255, nothing)
+cv.createTrackbar('UH', 'Tracking', 35, 255, utilities.nothing)
+cv.createTrackbar('US', 'Tracking', 255, 255, utilities.nothing)
+cv.createTrackbar('UV', 'Tracking', 255, 255, utilities.nothing)
 
-kernelOpen=np.ones((5,5))
-kernelClose=np.ones((9,9))
+prevCursorLoc = ()
+mouseDown = False
+
+frameCount = 0
+captureBGFlag = False
+remove_bg = False
+bg_ref = None
 
 while True:
     ret, frame = cap.read()
     if not ret:
         print("Couldn't retreive the frame. Exiting...")
         break
+    frameCount += 1
+
     cv.flip(frame,1,frame)
+
+    if captureBGFlag:
+        bg_ref = frame
+        print('Background Captured!')
+        #remove_bg = True
+        captureBGFlag = False
+    
+    if remove_bg:
+        if bg_ref is None:
+            print('Cannot Remove Background! Background Ref Not Set.')
+            remove_bg = False
+        else:
+            frame = utilities.remove_image_background(frame, bg_ref)
 
     img = frame
     cv.convertScaleAbs(frame, img, alpha=contrast/10, beta=brightness-128)
@@ -61,59 +95,106 @@ while True:
     us = cv.getTrackbarPos('US', 'Tracking')
     uv = cv.getTrackbarPos('UV', 'Tracking')
 
+    # Lowerbound and Upperbound for color detection
     l_b = np.array([lh,ls,lv])
     u_b = np.array([uh,us,uv])
 
-    processed = cv.GaussianBlur(img, (9,9), 0)
-    processed = cv.medianBlur(processed, 5)
-    processedHSV= cv.cvtColor(processed,cv.COLOR_BGR2HSV)
+    processedHSV= cv.cvtColor(utilities.preprocess_image(img), cv.COLOR_BGR2HSV)
     mask = cv.inRange(processedHSV, l_b, u_b)
-    mask = cv.dilate(mask, np.ones((5,5),np.uint8), iterations=3)
-    #morphology
-    maskOpen=cv.morphologyEx(mask, cv.MORPH_OPEN, kernelOpen)
-    maskClose=cv.morphologyEx(maskOpen, cv.MORPH_CLOSE, kernelClose)
+    
+    maskFinal = utilities.process_mask(mask)
+    contours, h = cv.findContours(maskFinal.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 
-    maskFinal = maskClose
-    conts,h = cv.findContours(maskFinal.copy(),cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
-    #print("Number of contours:" + str(len(conts)))
+    # Draw Tracking Area Range Box
+    #frame = cv.rectangle(frame, (trackingRangeX[0], trackingRangeY[0]), (trackingRangeX[1], trackingRangeY[1]), (0,0,255), 3)
 
-    #cv.drawContours(maskFinal, conts, -1, (255,0,0), 3)
-    boundingAreas = {}
-    for cnt in conts:
-        x,y,w,h = cv.boundingRect(cnt)
-        area = w*h
-        # Store area as key and the coordinates as value
-        boundingAreas[area] = [x,y,w,h,area]
+    boundingAreas = utilities.bounding_areas_from_contours(contours)
+
     # Sort the areas in descending order
-    boundingAreas = dict(sorted(boundingAreas.items(), key=lambda item: item[0], reverse=True))
+    boundingAreas = utilities.sort_dict_by_keys(boundingAreas, reverse=True)
     # Get the sorted bounding coordinates
     rects = list(boundingAreas.values())
     lenRects = len(rects)
     # Get only the firs two largest rectangles if there are more than two
     if lenRects > 2:
         lenRects = 2
+
     points = []
     for i in range(lenRects):
         x,y,w,h,area = rects[i]
+        # Discard if subsequent boxes are less than 20% of the largest box
         if i > 0 and area < 0.2*rects[0][4]:
             break
+        # Draw rectangle to show bounding box
         frame = cv.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 3)
         x_center = x+int(w/2)
         y_center = y+int(h/2)
         points.append((x_center, y_center))
+        # Draw circle to show center of bounding box
         frame = cv.circle(frame, (x_center, y_center), radius=5, color=(0,0,255), thickness=-1)
     
+    cursorLoc = ()
+    if len(points) == 1:
+        cursorLoc = points[0]
+    # If there are two bounding boxes, Draw a line through their centers
     if len(points) == 2:
         frame = cv.line(frame, points[0], points[1], color=(0,255,0), thickness=3)
         x_center = int((points[0][0] + points[1][0]) / 2)
         y_center = int((points[0][1] + points[1][1]) / 2)
-        frame = cv.circle(frame, (x_center, y_center), radius=5, color=(255,0,0), thickness=-1)
+        cursorLoc = (x_center, y_center)
+        # Draw circle to show center of the line
+        frame = cv.circle(frame, cursorLoc, radius=5, color=(255,0,0), thickness=-1)
+
+    currentMouseX, currentMouseY = pyautogui.position()
+    #if prevCursorLoc and cursorLoc:
+        #cursor_deviation_x = cursorLoc[0] - prevCursorLoc[0]
+        #cursor_deviation_y = cursorLoc[1] - prevCursorLoc[1]
+        #pyautogui.moveTo((currentMouseX + cursor_deviation_x) * sensitivity/100, (currentMouseY + cursor_deviation_y) * sensitivity/100)
+    
+    if enableMouseTracking:
+        if cursorLoc:
+            if len(points) == 1 and not mouseDown:
+                pyautogui.mouseDown()
+                mouseDown = True
+            elif len(points) == 2 and mouseDown:
+                pyautogui.mouseUp()
+                mouseDown = False
+            #if frameCount%5 == 0:
+            newMouseX = int(utilities.linear_interpolate(cursorLoc[0], trackingRangeX, [0, screenWidth]))
+            newMouseY = int(utilities.linear_interpolate(cursorLoc[1], trackingRangeY, [0, screenHeight]))
+            if frameCount%30 == 0:
+                print('Mouse:', newMouseX, newMouseY)
+                print('Screen:', screenWidth, screenHeight)
+                print('Cap:', cap.get(cv.CAP_PROP_FRAME_WIDTH), cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+            # Ignore micromovements
+            if abs(currentMouseX - newMouseX) > 2 or abs(currentMouseY - newMouseY) > 2:
+                pyautogui.moveTo(newMouseX, newMouseY)#, duration=0.5, tween=pyautogui.easeOutQuad)
+        elif len(points) == 0 and mouseDown:
+            pyautogui.mouseUp()
+            mouseDown = False
+
+    if cursorLoc:
+        prevCursorLoc = cursorLoc
     
     cv.imshow('frame', frame)
     #cv.imshow('mask', mask)
     cv.imshow('Processed Mask', maskFinal)
-    
-    if cv.waitKey(1) == ord('q'):
+
+    key = cv.waitKey(1)
+    if key == ord('m'):
+        enableMouseTracking = not enableMouseTracking
+        print('Mouse Tracking:', enableMouseTracking)
+    elif key == ord('b'):
+        print('Capturing Background...')
+        captureBGFlag = True
+    elif key == ord('r'):
+        if remove_bg:
+            print('Adding Background...')
+            remove_bg = False
+        else:
+            print('Removing Background...')
+            remove_bg = True
+    elif key == ord('q'):
         print('Exiting...')
         break
 
---
+++ utilities.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2 as cv
+
+kernelDilation = np.ones((5,5),np.uint8)
+kernelOpen = np.ones((5,5))
+kernelClose = np.ones((9,9))
+
+def preprocess_image(img):
+    processed = cv.GaussianBlur(img, (9,9), 0)
+    processed = cv.medianBlur(processed, 5)
+    return processed
+
+def process_mask(mask):
+    '''
+    Perform noise reduction and other processing for a given mask/image
+    '''
+    mask = cv.dilate(mask, kernelDilation, iterations=3)
+    #morphology
+    mask_open = cv.morphologyEx(mask, cv.MORPH_OPEN, kernelOpen)
+    mask_close = cv.morphologyEx(mask_open, cv.MORPH_CLOSE, kernelClose)
+    return mask_close
+
+def nothing(x=None):
+    # Literally does nothing
+    pass
+
+def bounding_areas_from_contours(contours):
+    '''
+    Takes in a list of contours and returns a dictionary whose keys are the bounding areas and values are the coordinates of that particular contour
+    '''
+    bounding_areas = {}
+    for cnt in contours:
+        x,y,w,h = cv.boundingRect(cnt)
+        area = w*h
+        # Store area as key and the coordinates as value
+        bounding_areas[area] = [x,y,w,h,area]
+    return bounding_areas
+
+def sort_dict_by_keys(data, reverse=False):
+    '''
+    Sort a dictionary by key
+    '''
+    return dict(sorted(data.items(), key=lambda item: item[0], reverse=reverse))
+
+def linear_interpolate(num, old_range, new_range):
+    '''
+    Perform linear interpolation for a number in a given range to a different range
+    '''
+    return ((new_range[1] - new_range[0]) * (num - old_range[0])/(old_range[1] - old_range[0])) + new_range[0]
+
+def remove_image_background(img, bg_ref):
+    diff1 = cv.subtract(img, bg_ref)
+    diff2 = cv.subtract(bg_ref, img)
+    diff = diff1 + diff2
+    diff[abs(diff) < 15.0] = 0
+    gray = cv.cvtColor(diff.astype(np.uint8), cv.COLOR_BGR2GRAY)
+    gray[np.abs(gray) < 10] = 0
+    fgmask = gray.astype(np.uint8)
+    fgmask[fgmask>0]=255
+    #invert the mask
+    #fgmask_inv = cv.bitwise_not(fgmask)
+    #use the masks to extract the FG
+    fgimg = cv.bitwise_and(img, img, mask = fgmask)
+    return fgimg
